1c602f8308f274acb0a4b5c1b30a110c60e381f5
dhimashary <dhimas.hary@gmail.com>
2023-06-02 22:21:03 +0700

    upload with apollo

---
+++ config/server.mjs
@@ -0,0 +1,51 @@
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { typeDefs, resolvers } from "../schema/test.mjs";
+import graphqlUploadExpress from "graphql-upload/graphqlUploadExpress.mjs";
+
+export default async function initServer(port = 4000) {
+  try {
+    const app = express();
+    const httpServer = http.createServer(app);
+
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+      uploads: false
+    });
+    await server.start();
+
+    app.use(
+      "/",
+      cors(),
+      bodyParser.json(),
+      graphqlUploadExpress(),
+      expressMiddleware(server, {
+        context: async ({ req }) => {
+          // decode token here if available
+          return {
+            id: 1,
+            username: 'asd',
+            role: 'admin'
+          };
+        },
+      })
+    );
+
+    // Modified server startup
+    await new Promise((resolve) => httpServer.listen({ port }, resolve));
+    const url = `http://localhost:${port}/`;
+    return {
+      server,
+      url
+    };
+  } catch (error) {
+    throw error;
+  }
+}
---
+++ schema/test.mjs
@@ -0,0 +1,61 @@
+import GraphQLUpload from "graphql-upload/GraphQLUpload.mjs";
+// import imagekit from '../helpers/imageUpload.mjs';
+import stream2buffer from '../helpers/streamToBuffer.mjs';
+
+export const typeDefs = `#graphql
+  scalar Upload
+  
+  type Image {
+    filename: String!
+  }
+
+  type UploadMessage {
+    msg: String
+  }
+
+  type Query {
+    images: [Image!]
+  }
+  
+  type Mutation {
+    uploadImages(images: [Upload!]!): UploadMessage
+  }
+`;
+
+
+export const resolvers = {
+  Upload: GraphQLUpload,
+  Query: {
+    images: () => images,
+  },
+  Mutation: {
+    uploadImages: async (_, { images }) => {
+      try {
+
+        const result = await Promise.all(images);
+        console.log(result, "-=-=-=");
+        const imagesBufferPromises = result.map(img => {
+          const stream = img.createReadStream();
+          return stream2buffer(stream);
+        });
+        const imagesBuffer = await Promise.all(imagesBufferPromises);
+
+        // sesuaikan upload dengan third party yg dipakai, karena imagekit hanya bisa upload satu file dalam satu waktu maka diambil dari index 0 saja
+        // const data = await imagekit.upload({
+        //   file: imagesBuffer[0],
+        //   fileName: result[0].filename
+        // });
+
+        // console.log(data);
+        // simpan data2 yg dibutuhkan seperti filename, url ke database kalian
+
+        return {
+          msg: "Upload success"
+        };
+      } catch (error) {
+        throw error;
+      }
+
+    },
+  },
+};
